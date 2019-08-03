5628cd4dbe1d170957f3a1435362254f4151b66b
ligsh <system6@jkchemical.com>
2019-08-03 17:38:43 +0800

    删除编译结果out文件夹

--- out/settings/bus/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const orderUsqBus_1 = require("./orderUsqBus");
-exports.bus = [
-    orderUsqBus_1.faceOrder,
-];
-//# sourceMappingURL=index.js.map
