8d5e5da25bd34259c95bc3638b25a1d947ffc512
ligsh <system6@jkchemical.com>
2019-12-30 14:11:05 +0800

    升级uq-joint,从1.0.12到1.0.19

--- out/log4jstest.js
+++
@@ -1,5 +0,0 @@
-var log4js = require('log4js');
-var logger = log4js.getLogger();
-logger.level = 'debug'; // default level is OFF - which means no logs at all.
-logger.debug("Some debug messages");
-//# sourceMappingURL=log4jstest.js.map
