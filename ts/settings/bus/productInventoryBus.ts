5d2e7c3175752b3c81d34f6bc71c929d4ec0c59b
ligsh <system6@jkchemical.com>
2020-02-10 15:51:41 +0800

    ligsh. 库存同步中将新构建的库存序列表作为数据源

--- ts/settings/bus/productInventoryBus.ts
+++ ts/settings/bus/productInventoryBus.ts
@@ -3,13 +3,12 @@ import { uqPullRead, uqOutRead } from "../../first/converter/uqOutRead";
 
 const productInventoryPull: DataPull<UqBus> = async (joint: Joint, uqBus: UqBus, queue: string | number): Promise<DataPullResult> => {
     let sql = `select top 1 wi.ID, wi.WarehouseID, j.jkid as ProductID, wi.PackagingID as PackingID, wi.Inventory
-        from ProdData.dbo.Export_WarehouseInventory wi inner join zcl_mess.dbo.jkcat j on wi.PackagingID = j.jkcat
+        from ProdData.dbo.Export_WarehouseInventory_queue wi inner join zcl_mess.dbo.jkcat j on wi.PackagingID = j.jkcat
         where wi.ID > @iMaxId order by wi.ID`;
     return await uqOutRead(sql, queue);
 }
 
 export const faceProductInventory: UqBus = {
-    // face: '百灵威系统工程部/point/productInventory',
     face: '百灵威系统工程部/warehouse/productInventory',
     from: 'local',
     mapper: {
